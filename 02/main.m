754390242ae65e4abfcc56f558557b8c0a4bbca5
Silvio98 <94136593+Silvio98@users.noreply.github.com>
2023-05-20 00:31:14 +0100

    Update main.m

--- 02/main.m
+++ 02/main.m
@@ -4,14 +4,16 @@ participant = '02';
 
 recording = input('Enter Recording: ', 's');
 
-data = cell(10, 2);  % Preallocate the cell array to store the results
+soundDataArray = cell(1, numFiles);
+sampleRateArray = zeros(1, numFiles);
 
 for i = 1 : 10
     disp(i);
     [soundData, sampleRate]  = loadRec(i-1, participant, recording);
     
-    data{i, 1} = soundData;  % Store the soundData value in the ith row, 1st column of the cell array
-    data{i, 2} = sampleRate;  % Store the sampleRate value in the ith row, 2nd column of the cell array
+    soundDataArray{i} = soundData;
+    sampleRateArray(i) = sampleRate;
+
 end
 
 soundDataArray = cell2mat(data(:, 1));
